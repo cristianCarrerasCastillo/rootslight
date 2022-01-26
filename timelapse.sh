271262aa83a48208a3c2f5a3887224e57ee29e4a
Cristian Carreras <cristian.carrerascastillo@gmail.com>
2022-01-25 23:00:21 -0300

    actualización de script controlador y de proyecto django

--- timelapse.sh
+++ timelapse.sh
@@ -1,8 +1,5 @@
 #l/bin/bash
-# Dev: Cristian Carreras
 DATE=$(date +"%d-%m-%Y_%H")
-echo 2 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio2/direction
-echo 1 > /sys/class/gpio/gpio2/value
+python3 /home/pi/Pictures/timelapse/on.py
 libcamera-jpeg -o /home/pi/Pictures/timelapse/$DATE.jpg
-echo 0 > /sys/class/gpio/gpio2/value 
+python3 /home/pi/Pictures/timelapse/off.py
