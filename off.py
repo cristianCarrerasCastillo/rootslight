79982abc5b0e574bab0bcf48a280ff44647f2811
Cristian Carreras <cristian.carrerascastillo@gmail.com>
2022-01-30 01:04:34 -0300

    hotfix gpio workup randomly

--- off.py
+++ off.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 #Definimos el modo BCM
 GPIO.setmode(GPIO.BCM)
 #Ahora definimos el pin GPIO 17 como salida
-GPIO.setup(4, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
 #Y le damos un valor logico bajo para apagar el LED
 GPIO.output(4, GPIO.LOW)
 #Finalmente liberamos todos los pines GPIO, es decir, los desconfiguramos)
